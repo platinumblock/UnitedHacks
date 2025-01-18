16e12f2e537ec06b0e941eba7091e78b74d86cc3
Jonathan Uhler <jonathan@uhler.com>
2025-01-18 15:36:44 -0800

    add street click detection

--- map.js
+++ map.js
@@ -1,12 +1,19 @@
-var points = []
-function detect(event){
-    var x = event.clientX;
-    var y = event.clientY;
-    var dot = document.createElement("div");
-    dot.classList.add("dot");
-    dot.style.left = x + "px";
-    dot.style.top = y + "px";
-    points.push([x, y])
-    console.log(points);
-    document.body.appendChild(dot);
+function detect(event) {
+    let x = event.clientX;
+    let y = event.clientY;
+    const delta = 15;
+
+    for (let streetName in CITIES.mountainView) {
+        let street = CITIES.mountainView[streetName];
+        let points = street.points;
+        for (let i = 0; i < points.length; i++) {
+            let point = points[i];
+            let px = point[0];
+            let py = point[1];
+            if (x >= px - delta && x <= px + delta && y >= py - delta && y <= py + delta) {
+                console.log(street);
+                return street;
+            }
+        }
+    }
 }
