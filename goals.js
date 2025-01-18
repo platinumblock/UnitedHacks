6c18bd4efdf28fb238f9eda1c9e8cbfd1eba10a2
Jonathan Uhler <jonathan@uhler.com>
2025-01-17 23:10:02 -0800

    add js files

---
+++ goals.js
@@ -0,0 +1,19 @@
+class Goal {
+
+    constructor() {
+
+    }
+
+}
+
+
+/**
+ * Recommends a new goal based on a list of activities that the user has performed recently.
+ *
+ * @param activities  a list of {@code Activity} objects (see activity.js).
+ *
+ * @return a {@code Goal} object for the user to perform.
+ */
+export function generateGoal(activities) {
+
+}
