659503e56bf238f461736585707233df63a7f620
Jonathan Uhler <jonathan@uhler.com>
2025-01-18 16:33:06 -0800

    fix dots

--- map.js
+++ map.js
@@ -1,12 +1,16 @@
 function drawDots() {
+    let canvas = document.getElementById("map");
+    let width = canvas.width;
+    let height = canvas.height;
+
     for (let streetName in CITIES.mountainView) {
         let street = CITIES.mountainView[streetName];
         console.log(streetName + ": " + street.street.getTransitImportanceStr(new Date()));
         let points = street.points;
         for (let i = 0; i < points.length; i++) {
             let point = points[i];
-            let px = point[0];
-            let py = point[1];
+            let px = Math.round(point[0] / 1388 * width);
+            let py = Math.round(point[1] / 750 * height);
 
             let dot = document.createElement("div");
             dot.classList.add("dot");
@@ -21,14 +25,6 @@ function drawDots() {
 function detect(event) {
     let x = event.clientX;
     let y = event.clientY;
-
-    console.log("[" + x + ", " + y + "],");
-
-            let dot = document.createElement("div");
-            dot.classList.add("dot");
-            dot.style.left = x + "px";
-            dot.style.top = y + "px";
-            document.body.appendChild(dot);
     const delta = 15;
 
     for (let streetName in CITIES.mountainView) {
