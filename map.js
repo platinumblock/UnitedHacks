ccbb83934a8b1aee4d3da1389d2b19b83e03f941
Jonathan Uhler <jonathan@uhler.com>
2025-01-18 16:36:52 -0800

    fix click

--- map.js
+++ map.js
@@ -26,14 +26,17 @@ function detect(event) {
     let x = event.clientX;
     let y = event.clientY;
     const delta = 15;
+    let canvas = document.getElementById("map");
+    let width = canvas.width;
+    let height = canvas.height;
 
     for (let streetName in CITIES.mountainView) {
         let street = CITIES.mountainView[streetName];
         let points = street.points;
         for (let i = 0; i < points.length; i++) {
             let point = points[i];
-            let px = point[0];
-            let py = point[1];
+            let px = Math.round(point[0] / 1388 * width);
+            let py = Math.round(point[1] / 750 * height);
             if (x >= px - delta && x <= px + delta && y >= py - delta && y <= py + delta) {
                 return street;
             }
