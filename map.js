725fadcddb3cbd82ce5d92f2e774c9fd6a9714b1
Jonathan Uhler <jonathan@uhler.com>
2025-01-18 16:14:43 -0800

    add more streets

--- map.js
+++ map.js
@@ -1,6 +1,34 @@
+function drawDots() {
+    for (let streetName in CITIES.mountainView) {
+        let street = CITIES.mountainView[streetName];
+        console.log(streetName + ": " + street.street.getTransitImportanceStr(new Date()));
+        let points = street.points;
+        for (let i = 0; i < points.length; i++) {
+            let point = points[i];
+            let px = point[0];
+            let py = point[1];
+
+            let dot = document.createElement("div");
+            dot.classList.add("dot");
+            dot.style.left = px + "px";
+            dot.style.top = py + "px";
+            document.body.appendChild(dot);
+        }
+    }
+}
+
+
 function detect(event) {
     let x = event.clientX;
     let y = event.clientY;
+
+    console.log("[" + x + ", " + y + "],");
+
+            let dot = document.createElement("div");
+            dot.classList.add("dot");
+            dot.style.left = x + "px";
+            dot.style.top = y + "px";
+            document.body.appendChild(dot);
     const delta = 15;
 
     for (let streetName in CITIES.mountainView) {
@@ -11,9 +39,11 @@ function detect(event) {
             let px = point[0];
             let py = point[1];
             if (x >= px - delta && x <= px + delta && y >= py - delta && y <= py + delta) {
-                console.log(street);
                 return street;
             }
         }
     }
 }
+
+
+drawDots();
